8404515dc9d66633b4190544743e7bb0d64ff604
Anshuman Airy <anshuman.airy04@gmail.com>
2020-07-16 16:29:54 +0530

    Initialize Skeleton

---
+++ freight/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FreightConfig(AppConfig):
+    name = 'freight'
---
+++ freight/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from freight.controller import admin
+from django.conf.urls import url,include
+
+from freight.controller import index
+from freight.controller import about
+from freight.controller import services
+from freight.controller import error
+from freight.controller import contact
+
+urlpatterns = [
+    url(r'^admin',admin.admin,name="admin"),
+
+    url(r'^$',index.index,name="index"),
+    url(r'^about',about.about,name="about"),
+    url(r'^services',services.services,name="services"),
+    url(r'^error',error.error,name="error"),
+    url(r'^contact',contact.contact,name="contact"),
+    # url(r'^privacy',privacy.privacy,name="privacy"),
+    # url(r'^terms',terms.terns,name="terms"),
+]
