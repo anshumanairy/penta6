3b3fa881d694f49c127eeab39a2984b796c6a1c0
tanu2801 <swastiktanuagrawal805@gmail.com>
2020-07-27 04:42:42 -0400

    contact

---
+++ freight/forms/contactform.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length=100, widget= forms.TextInput
+                           (attrs={'placeholder':'Eg.John Mathew','style': 'width:600px'}))
+    email = forms.EmailField( widget= forms.EmailInput
+                           (attrs={'placeholder':'Eg. example@example.com','style': 'width:600px'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Message...','style': 'width:600px'}))
