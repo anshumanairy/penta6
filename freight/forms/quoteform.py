2ed5ae10a1fdce05c3a79a8bbabae7fd3309c62c
Navneetkumar13 <navneet1465@gmail.com>
2020-07-29 19:46:53 +0530

    quote form and contact form minor edits

--- freight/forms/quoteform.py
+++ freight/forms/quoteform.py
@@ -1,5 +1,5 @@
 from django import forms
-from freight.models.quotes import Quotes 
+from freight.models.quotes import Quotes
 
 class QuoteForm(forms.ModelForm):
     def get_country():
@@ -9,26 +9,26 @@ class QuoteForm(forms.ModelForm):
         else:
             raise forms.ValidationError("Contact Owner no Country found")
 
-    name=forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Your Name','style':'display:block;'}))
-    email=forms.EmailField(widget=forms.TextInput(attrs={'placeholder':'Your Email'}))
-    goods=forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Goods'}))
-    quantity=forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Enter Quantity'}))
-    c_type=forms.ChoiceField(choices=Quotes.types,label='Type')
+    name=forms.CharField(widget=forms.TextInput(attrs={'id':'form1','class':'form-control','placeholder':'Enter Your Name','style':'display:block;background:rgba(0,0,0,0.8);border-radius:5px;color:white;'}))
+    email=forms.EmailField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Your Email','style':'background:rgba(0,0,0,0.8);border-radius:5px;color:white;'}))
+    goods=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Goods','style':'background:rgba(0,0,0,0.8);border-radius:5px;color:white;'}))
+    quantity=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Quantity','style':'background:rgba(0,0,0,0.8);border-radius:5px;color:white;'}))
+    c_type=forms.ChoiceField(choices=Quotes.types,label='Type',widget=forms.Select(attrs={'class':'form-control','placeholder':'Type','style':'background:rgba(0,0,0,0.8);border-radius:5px;border-color:black;color:gray;width:100%;'}))
     try:
-        destination=forms.ChoiceField(choices=get_country(),label='Destination')
+        destination=forms.ChoiceField(choices=get_country(),label='Destination',widget=forms.Select(attrs={'class':'form-control','placeholder':'Destination','style':'background:rgba(0,0,0,0.8);border-color:black;border-radius:5px;color:gray;width:100%;'}))
     except:
         pass
-    destination_address=forms.CharField(widget=forms.Textarea(attrs={'placeholder':'Enter Destination Address'}))
-    destination_pincode=forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder':'Enter Destination Pin'}))
+    destination_address=forms.CharField(widget=forms.Textarea(attrs={'class':'form-control','placeholder':'Enter Destination Address','style':'resize:none;background:rgba(0,0,0,0.8);border-radius:5px;color:white;'}))
+    destination_pincode=forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control','placeholder':'Enter Destination Pin','style':'background:rgba(0,0,0,0.8);border-radius:5px;color:white;'}))
     try:
-        origin=forms.ChoiceField(choices=get_country(),label='Origin')
+        origin=forms.ChoiceField(choices=get_country(),label='Origin',widget=forms.Select(attrs={'class':'form-control','placeholder':'Origin','style':'background:rgba(0,0,0,0.8);border-radius:5px;border-color:black;color:gray;width:100%;'}))
     except:
         pass
-    origin_address=forms.CharField(widget=forms.Textarea(attrs={'placeholder':'Enter Source Address'}))
-    origin_pincode=forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder':'Enter Source Pin'}))
-    ctr_code=forms.ChoiceField(choices=Quotes.cnt_codes,label='Country Code')
-    phone=forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Your Phone'}))
-    q_message=forms.CharField(widget=forms.Textarea(attrs={'placeholder':'Your Message...'}),label='Message')
+    origin_address=forms.CharField(widget=forms.Textarea(attrs={'class':'form-control','placeholder':'Enter Source Address','style':'resize:none;background:rgba(0,0,0,0.8);border-radius:5px;color:white;'}))
+    origin_pincode=forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control','placeholder':'Enter Source Pin','style':'background:rgba(0,0,0,0.8);border-radius:5px;color:white;'}))
+    ctr_code=forms.ChoiceField(choices=Quotes.cnt_codes,label='Country Code',widget=forms.Select(attrs={'class':'form-control','placeholder':'Country Code','style':'background:rgba(0,0,0,0.8);border-radius:5px;border-color:black;color:gray;width:100%;'}))
+    phone=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Your Phone','style':'background:rgba(0,0,0,0.8);border-radius:5px;color:white;'}))
+    q_message=forms.CharField(widget=forms.Textarea(attrs={'class':'form-control','placeholder':'Your Message...','style':'resize:none;background:rgba(0,0,0,0.8);border-radius:5px;color:white;'}),label='Message')
     class Meta:
         model=Quotes
         fields="__all__"
